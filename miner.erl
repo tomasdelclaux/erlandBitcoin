5e1985e3e8ae1507baaf58b40bc3d61a211f3318
arielweitz <arielw1505@gmail.com>
2022-09-19 13:27:45 -0400

    example 1 done

--- miner.erl
+++ miner.erl
@@ -2,28 +2,21 @@
 
 -module(miner).
 -author("Tomas Delclaux").
--compile(export_all).
+-export([main/0]).
+-define(GATOR, "tomas.delclauxro;").
 
-% io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,
-%  "COP5615 is a boring class"))]).
-
-prefix([], _) -> true;
-prefix([Ch | Rest1], [Ch | Rest2]) ->
-        prefix(Rest1, Rest2);
-prefix(_, _) -> false.
-
-
-find_hash(true, [String]) -> io:format("mined string: ~s~n", [String]).
 find_hash(Num) ->  
     ZeroString = lists:duplicate(Num, "0"),
-    RString = "tomas.delclauxro;" ++ base64:encode(crypto:strong_rand_bytes(8)),
-    Res=prefix(lists:flatten(ZeroString)    
-                ,io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,RString))])),
-    case Res of
-        true -> io:format("FOUND: ~s~n", [io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,RString))])]),find_hash(true, [RString]);
-        false -> io:format("NOT FOUND~n"),find_hash(Num)
+    RString = ?GATOR ++ base64:encode_to_string(crypto:strong_rand_bytes(8)),
+    Hashed = binary:decode_unsigned(crypto:hash(sha256,RString)),
+    HashedString = io_lib:format("~64.16.0b", [Hashed]),
+    Test = string:prefix(HashedString, ZeroString),
+    case Test of
+        nomatch -> find_hash(Num);
+        _ -> [RString ++ "\t" ++ HashedString]
     end.
 
-lets_go() ->
-    {ok,[Num]} = io:fread("Enter Number of Zeroes:", "~d"),
-    find_hash(Num).
+main() ->
+    {ok,[Num]} = io:fread("", "~d"),
+    Res = find_hash(Num),
+    io:format("~s\n", Res).
