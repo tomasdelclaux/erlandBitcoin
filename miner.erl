aa62eb26988ddee4dbad2e731011bc5a37b4f557
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2022-09-19 16:20:21 -0400

    changed rand bytes to 24
    This removes the constraint that the last character is an = sign

--- miner.erl
+++ miner.erl
@@ -7,7 +7,7 @@
 
 find_hash(Num) ->  
     ZeroString = lists:duplicate(Num, "0"),
-    RString = ?GATOR ++ base64:encode_to_string(crypto:strong_rand_bytes(8)),
+    RString = ?GATOR ++ base64:encode_to_string(crypto:strong_rand_bytes(24)),
     Hashed = binary:decode_unsigned(crypto:hash(sha256,RString)),
     HashedString = io_lib:format("~64.16.0b", [Hashed]),
     Test = string:prefix(HashedString, ZeroString),
